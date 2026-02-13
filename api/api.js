fe5c88fd490d345344b22fa385f903589c47643a
Lian Dev <liancarvalhodacosta@gmail.com>
2026-02-13 20:50:58 -0300

    Update API data

--- api/api.js
+++ api/api.js
@@ -2,10 +2,10 @@ import DataBase from "./config"
 
 const API = new DataBase();
 
-API.createNewAccount(0, 'liandev', 'Lian Dev', 'Ceará, Brazil', 'Desenvolvedor e Designer', 'liandev.png', 360);
-API.createNewAccount(1, 'socialapp', 'Social App', 'Ceará, Brazil', 'Novo projeto que saiu do forno (Social App)', 'sociallogo.png', 160);
-API.createNewAccount(2, 'barbershop', 'Barbearia', 'Ceará, Brazil', 'Novo projeto que saiu do forno (Barbearia)', 'barbericon.png', 300);
-API.createNewAccount(3, 'movebusiness', 'Move', 'Ceará, Brazil', 'Novo projeto vindo (Move)', 'moveprofile.png', 0);
+API.createNewAccount(0, 'liandev', 'Lian Dev', 'Ceará, Brazil', 'Desenvolvedor e Designer', 'https://github.com/lian-carvalho', 'liandev.png', 360);
+API.createNewAccount(1, 'socialapp', 'Social App', 'Ceará, Brazil', 'Novo projeto que saiu do forno (Social App)', '', 'sociallogo.png', 160);
+API.createNewAccount(2, 'barbershop', 'Barbearia', 'Ceará, Brazil', 'Novo projeto que saiu do forno (Barbearia)', 'https://barbearia-barber.vercel.app/', 'barbericon.png', 300);
+API.createNewAccount(3, 'movebusiness', 'Move', 'Ceará, Brazil', 'Novo projeto vindo (Move)', '', 'moveprofile.png', 0);
 
 API.createNewPost(0, 0, 'socialbanner.png', 'Desenvolvendo o Social App', 'Processo de desenvolvimento do app', 'há 10 horas', false, undefined, true);
 API.createNewPost(1, 0, 'liandevbanner.png', 'Nova landing page', 'Querendo um website apenas seu? Só encomendar comigo', 'há 2 horas', true, 0);
