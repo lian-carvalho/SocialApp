b13de56fd6dba541b9393e53420736bf166431c5
Lian Dev <liancarvalhodacosta@gmail.com>
2026-02-21 14:40:27 -0300

    New Search page

---
+++ app/search/page.tsx
@@ -0,0 +1,9 @@
+import SearchModule from "@/components/section/SearchModule"
+
+export default function Search() {
+    return (
+        <div className="screen-container">
+            <SearchModule />
+        </div>
+    )
+}
