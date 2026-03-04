e7e0b2822577dbe7ba2bcef9047021fc1112848a
Lian Dev <liancarvalhodacosta@gmail.com>
2026-03-04 11:07:56 -0300

    New NotFound page

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+import NotFoundDisplay from "@/components/section/NotFoundDisplay"
+
+export default function NotFound() {
+    return (<div className="screen-container">
+        <NotFoundDisplay />
+    </div>)
+}
