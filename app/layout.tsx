6eb07e048fd5d38a17241ecade439898c605706f
Lian Dev <liancarvalhodacosta@gmail.com>
2026-03-06 19:43:29 -0300

    Update SEO(layout Metadata and new banner)

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,15 +13,51 @@ const inter = Inter({
 
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://devsocialapp.vercel.app"),
+
   title: "SocialApp",
-  description: "A portfolio project developed by Lian Dev",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    viewportFit: "cover",
-  },
+  description: "A rede social do momento | A portfolio project developed by Lian Dev",
   icons: {
     icon: "/favicon.ico",
+  },
+
+  keywords: ["rede social", "socialapp", "projeto", "project", "portfolio", "liandev", "desenvolvedor", "developer"],
+
+  openGraph: {
+    title: "Social App",
+    description: "A rede social do momento - Portifolio project",
+    url: "https://devsocialapp.vercel.app",
+    siteName: "Social App",
+    images: [
+      {
+        url: '/seo/banner.png',
+        width: 1200,
+        height: 630,
+        alt: 'Rede social'
+      }
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Barbearia",
+    description: "A rede social do momento - Portifolio project",
+    images: ["/seo/banner.png"],
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-snippet": -1,
+      "max-image-preview": "large",
+      "max-video-preview": -1,
+    }
   },
 };
 
@@ -31,7 +67,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
         className={`${inter.variable} antialiased flex justify-center overflow-hidden bg-gray1 h-[100dvh]`}>
         <div className="flex flex-col bg-black grow max-w-md h-full overflow-hidden">
